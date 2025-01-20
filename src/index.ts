9c3cdda591af771d8ce24d71bd2b5bc52b13ec17
poisnada <horberlan@gmail.com>
2025-01-20 13:28:08 -0300

    feat: setup api structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/cep/:id", ({ params: { id } }) => {
+    
+  })
+  .listen(3000);
+
+// https://cdn.apicep.com/file/apicep/${id}.json
+// https://cep.awesomeapi.com.br/json/${id}
+// https://viacep.com.br/ws/${id}/json/
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
